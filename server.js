7205dc66c858b31a85dfd41aee74f85698a342b5
Shiv9834 <76648664+Shiv69092@users.noreply.github.com>
2024-09-12 02:28:50 +0530

    Fix CORS and root route

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');  // Import CORS
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Middleware to parse incoming JSON data
 app.use(express.json());
@@ -12,19 +16,21 @@ const userRoutes = require('./routes/userRoutes');
 // Use routes
 app.use('/users', userRoutes);
 
-const User = require('./models/userModel');
+// Add a simple route for the root URL to avoid "Cannot GET /" errors
+app.get('/', (req, res) => {
+  res.send('Welcome to the backend API!');
+});
 
 // Connect to MongoDB
 const mongoURI = 'mongodb+srv://user_0912:Shivanshu@cluster0.jcoqs.mongodb.net/userDetails?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect(mongoURI)
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB connected');
   })
   .catch(err => {
     console.error('MongoDB connection error:', err);
   });
-
 
 // Start the server
 app.listen(port, () => {
